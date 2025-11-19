c5433e4da2c90cfce10cb67eb869b189e6ac2aed
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-19 16:47:30 +0700

    feat: add compare password function to check the user password

--- src/lib/password.ts
+++ src/lib/password.ts
@@ -3,3 +3,10 @@ import bcrypt from "bcrypt";
 export async function hashPassword(password: string) {
   return await bcrypt.hash(password, 10);
 }
+
+export async function comparePassword(
+  plainPassword: string,
+  hashedPassword: string
+) {
+  return await bcrypt.compare(plainPassword, hashedPassword);
+}
