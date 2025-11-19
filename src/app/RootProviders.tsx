506a757366b14128ccc80acd815e311a5412179b
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-19 16:46:59 +0700

    feat: add Session Provider

--- src/app/RootProviders.tsx
+++ src/app/RootProviders.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { ThemeProvider } from "@/hooks/useTheme";
+import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
 export function RootProviders({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
-      <Toaster position="top-right" />
-      {children}
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider>
+        <Toaster position="top-right" />
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
