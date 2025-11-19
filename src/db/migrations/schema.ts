108d31bac6ce7baf255d225e467a154c80ea4b8c
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-19 16:00:56 +0700

    feat: generated migrations file from dizzle

--- src/db/migrations/schema.ts
+++ src/db/migrations/schema.ts
@@ -1,15 +1,14 @@
-import { pgTable, unique, text, varchar, timestamp } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
+import { pgTable, unique, text, varchar, timestamp } from "drizzle-orm/pg-core";
 
-
-
-export const users = pgTable("users", {
-	id: text().primaryKey().notNull(),
-	email: varchar({ length: 256 }).notNull(),
-	password: text().notNull(),
-	name: text(),
-	role: varchar({ length: 50 }).default('student'),
-	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow(),
-}, (table) => [
-	unique("users_email_unique").on(table.email),
-]);
+export const users = pgTable(
+  "users",
+  {
+    id: text().primaryKey().notNull(),
+    email: varchar({ length: 256 }).notNull(),
+    password: text().notNull(),
+    name: text(),
+    role: varchar({ length: 50 }).default("student"),
+    createdAt: timestamp("created_at", { mode: "string" }).defaultNow(),
+  },
+  (table) => [unique("users_email_unique").on(table.email)]
+);
