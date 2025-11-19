3ef7baa4ee287d878140383022367ad10aff6023
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-19 16:46:44 +0700

    feat: implement the root provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "@/hooks/useTheme";
-import { Toaster } from "react-hot-toast";
+import { RootProviders } from "./RootProviders";
 
 export const metadata: Metadata = {
   title: "Course Platform",
@@ -9,16 +8,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen bg-background text-foreground antialiased">
-        <ThemeProvider>
-          <Toaster position="top-right"/>
-          {children}
-        </ThemeProvider>
+        <RootProviders>{children}</RootProviders>
       </body>
     </html>
   );
