f43cb8a5f9a88d0f3f9965dfcaedcf092fefbd25
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-20 16:08:05 +0700

    feat: create schema for user progress

---
+++ src/db/schema/progress.ts
@@ -0,0 +1,23 @@
+import {
+  boolean,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { lessons } from "./lessons";
+import { users } from "./users";
+
+export const progress = pgTable("progress", {
+  progressId: text("progressId").primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.userId),
+  lessonId: text("lessionId")
+    .notNull()
+    .references(() => lessons.lessonId),
+  completed: boolean("completed").default(false),
+  completedAt: timestamp("completedAt"),
+  createdAt: timestamp("createdAt").defaultNow(),
+  updatedAt: timestamp("updatedAt").defaultNow(),
+});
