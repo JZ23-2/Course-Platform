62f7312c98f5dfa51fb8a9b605c0d054f3fbd157
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-20 19:51:32 +0700

    feat: refactor all the logic in this page into useAdminCourses hooks and implement it

--- src/app/admin/course/page.tsx
+++ src/app/admin/course/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import Navbar from "@/components/ui/navbar";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
@@ -16,123 +16,32 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Trash, Edit, Eye, Plus, Search, MoreHorizontal } from "lucide-react";
 import { motion } from "framer-motion";
-import { createCourseInterface } from "@/interface/admin/courses/create-course-interface";
-import {
-  createCourseAction,
-  deleteCourseAction,
-  getAllCoursesAction,
-  updateCourseAction,
-} from "@/actions/admin/courses-services";
-import toast from "react-hot-toast";
-import { GetCourseInterface } from "@/interface/admin/courses/get-course-interface";
 import DeleteModal from "@/components/ui/delete-modal";
 import { CourseModal } from "@/components/ui/courses/CourseModal";
+import { useAdminCourses } from "@/hooks/useAdminCourses";
 
 export default function AdminCoursesDashboard() {
   const router = useRouter();
-  const [courses, setCourses] = useState<GetCourseInterface[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [openDialog, setOpenDialog] = useState(false);
-  const [editingCourse, setEditingCourse] = useState<GetCourseInterface | null>(
-    null
-  );
-  const [formData, setFormData] = useState<createCourseInterface>({
-    title: "",
-    description: "",
-    thumbnail: "",
-    sortOrder: 0,
-    status: "Draft",
-    type: "",
-  });
-  const [search, setSearch] = useState("");
-  const [deleteDialog, setDeleteDialog] = useState(false);
-  const [courseToDelete, setCourseToDelete] =
-    useState<GetCourseInterface | null>(null);
 
-  useEffect(() => {
-    loadCourses();
-  }, []);
-
-  const loadCourses = async (q: string = "") => {
-    setLoading(true);
-    const test = await getAllCoursesAction(q);
-    setCourses(test);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    const t = setTimeout(() => loadCourses(search), 300);
-    return () => clearTimeout(t);
-  }, [search]);
-
-  const openCreate = () => {
-    setEditingCourse(null);
-    setFormData({
-      title: "",
-      description: "",
-      thumbnail: "",
-      sortOrder: 0,
-      status: "Draft",
-      type: "Free",
-    });
-    setOpenDialog(true);
-  };
-
-  const openEdit = (course: GetCourseInterface) => {
-    setEditingCourse(course);
-    setFormData({
-      title: course.title,
-      description: course.description ?? "",
-      thumbnail: course.thumbnail ?? "",
-      sortOrder: course.sortOrder ?? 0,
-      status: course.status ?? "Draft",
-      type: course.type ?? "Free",
-    });
-    setOpenDialog(true);
-  };
-
-  const handleSave = async () => {
-    if (editingCourse) {
-      const res = await updateCourseAction(editingCourse.courseId, formData);
-
-      if (res.success) {
-        toast.success(res.message);
-      } else {
-        toast.error(res.message);
-      }
-    } else {
-      const res = await createCourseAction(formData);
-
-      if (res.success) {
-        toast.success(res.message);
-      } else {
-        toast.error(res.message);
-      }
-    }
-    setOpenDialog(false);
-    loadCourses(search);
-  };
-
-  const confirmDelete = (course: GetCourseInterface) => {
-    setCourseToDelete(course);
-    setDeleteDialog(true);
-  };
-
-  const handleDeleteConfirmed = async () => {
-    if (!courseToDelete) return;
-
-    const res = await deleteCourseAction(courseToDelete.courseId);
-
-    if (res.success) {
-      toast.success(res.message);
-    } else {
-      toast.error(res.message);
-    }
-
-    setDeleteDialog(false);
-    setCourseToDelete(null);
-    loadCourses(search);
-  };
+  const {
+    search,
+    setSearch,
+    openCreate,
+    loading,
+    courses,
+    openEdit,
+    confirmDelete,
+    courseToDelete,
+    deleteDialog,
+    editingCourse,
+    formData,
+    handleDeleteConfirmed,
+    handleSave,
+    openDialog,
+    setDeleteDialog,
+    setFormData,
+    setOpenDialog,
+  } = useAdminCourses();
 
   return (
     <div className="min-h-screen bg-linear-to-b from-background/60 via-background to-background p-0">
