4e8425b61761efacf64e9c5bcf29039b7ac24588
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-21 07:52:28 +0700

    feat: add lesson interface

---
+++ src/interface/admin/lessons/lesson-interface.ts
@@ -0,0 +1,12 @@
+export interface LessonInterface {
+  lessonId: string;
+  chapterId?: string;
+  title: string | null;
+  description: string | null;
+  content: string | null;
+  type: string | null;
+  videoUrl: string | null;
+  sortOrder: number | null;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+}
