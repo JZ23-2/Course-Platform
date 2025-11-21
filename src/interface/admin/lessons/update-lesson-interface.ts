3a4c21272fa679ec8c740cffcd44e2af4baf5c4d
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-21 13:31:15 +0700

    feat: add interface to update lesson

---
+++ src/interface/admin/lessons/update-lesson-interface.ts
@@ -0,0 +1,12 @@
+export interface updateLessonsInterface {
+  lessonId?: string;
+  chapterId?: string;
+  title?: string;
+  type?: string;
+  videoUrl?: string;
+  description?: string;
+  content?: string;
+  sortOrder?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
