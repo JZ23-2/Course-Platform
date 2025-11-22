b3dee22fbe1d5f5063da2f0ef9d4a69a7edeeee5
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-22 22:54:15 +0700

    update: naming convention

--- src/db/schema/quiz_options.ts
+++ src/db/schema/quiz_options.ts
@@ -2,12 +2,15 @@ import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { quiz_questions } from "./quiz_questions";
 
 export const quiz_options = pgTable("quiz_options", {
-  quizOptionId: text("id").primaryKey(),
+  quizOptionId: text("quizOptionId").primaryKey(),
   quizQuestionId: text("quizQuestionId")
     .notNull()
-    .references(() => quiz_questions.quizQuestionId),
-  option_text: text("option_text").notNull(),
-  is_correct: boolean("is_correct").default(false),
-  created_at: timestamp("created_at").defaultNow(),
-  updated_at: timestamp("updated_at").defaultNow(),
+    .references(() => quiz_questions.quizQuestionId, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+  optionText: text("optionText").notNull(),
+  isCorrect: boolean("isCorrect").default(false),
+  createdAt: timestamp("createdAt").defaultNow(),
+  updatedAt: timestamp("updatedAt").defaultNow(),
 });
