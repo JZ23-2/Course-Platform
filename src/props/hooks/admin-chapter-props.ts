57ca19a5e42a238bf0d6848a16fc0c1980de64fc
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-22 16:34:20 +0700

    feat: refactor some general chapter logic related to use chapter hooks

--- src/props/hooks/admin-chapter-props.ts
+++ src/props/hooks/admin-chapter-props.ts
@@ -1,5 +1,10 @@
+import { getChapterInterface } from "@/interface/admin/chapters/get-chapter-interface";
 import { GetCourseInterface } from "@/interface/admin/courses/get-course-interface";
+import { Dispatch, SetStateAction } from "react";
 
 export interface adminChapterProps {
-  courseDetail: GetCourseInterface | null;
+  courseDetail?: GetCourseInterface | null;
+  chapters?: getChapterInterface[];
+  setChapters?: Dispatch<SetStateAction<getChapterInterface[]>>;
+  fetchChaptersDetail?: () => Promise<void>;
 }
