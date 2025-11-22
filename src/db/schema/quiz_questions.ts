026f59ee7c38d121fcfe6e5125e73b43764ef205
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-22 22:54:11 +0700

    update: naming convention

--- src/db/schema/quiz_questions.ts
+++ src/db/schema/quiz_questions.ts
@@ -2,10 +2,13 @@ import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { quizzes } from "./quizzes";
 
 export const quiz_questions = pgTable("quiz_questions", {
-  quizQuestionId: text("id").primaryKey(),
+  quizQuestionId: text("quizQuestionId").primaryKey(),
   quizId: text("quizId")
     .notNull()
-    .references(() => quizzes.quizId),
+    .references(() => quizzes.quizId, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
   question: text("question").notNull(),
   points: integer("points").default(1),
   created_at: timestamp("created_at").defaultNow(),
