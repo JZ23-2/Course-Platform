fa8f615b0ef092259dbcbc783a88aa74faa3da59
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-22 09:58:13 +0700

    feat: add user provider in the root provider

--- src/app/RootProviders.tsx
+++ src/app/RootProviders.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ThemeProvider } from "@/hooks/useTheme";
+import { UserProvider } from "@/hooks/useUser";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
@@ -8,8 +9,10 @@ export function RootProviders({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider>
-        <Toaster position="top-right" />
-        {children}
+        <UserProvider>
+          <Toaster position="top-right" />
+          {children}
+        </UserProvider>
       </ThemeProvider>
     </SessionProvider>
   );
