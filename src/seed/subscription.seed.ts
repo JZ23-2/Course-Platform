f14824c3b2802f28e56a96d3424b25e13a3ce80e
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-23 15:37:13 +0700

    feat: add subsription seed

--- src/seed/subscription.seed.ts
+++ src/seed/subscription.seed.ts
@@ -1,11 +1,10 @@
 import { db } from "@/db/drizzle";
 import { subscriptions } from "@/db/schema";
-import { nanoid } from "nanoid";
 
 export async function seedSubscriptions() {
   await db.insert(subscriptions).values({
-    subscriptionId: nanoid(),
-    userId: "a7aac8b0-403e-46aa-ad13-9b8c911a5c2a",
+    subscriptionId: "subscription123",
+    userId: "user1",
     plan: "monthly",
     status: "active",
     startDate: new Date(),
