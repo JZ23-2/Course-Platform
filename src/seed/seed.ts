c6357481edb17c7f2bec9640b5de962e37b0bf53
JZ23-2 <jacksonzhang077@gmail.com>
2025-11-23 15:38:01 +0700

    update: seed for all model

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,7 +1,20 @@
 import { seedSubscriptions } from "./subscription.seed";
 import "dotenv/config";
+import { seedUsers } from "./user.seed";
+import { seedCourses } from "./course.seed";
+import { seedChapters } from "./chapter.seed";
+import { seedLessons } from "./lesson.seed";
+import { seedQuizzes } from "./quiz.seed";
+import { seedQuizQuestions } from "./quiz-question.seed";
 
 async function main() {
+  await seedUsers();
+  await seedCourses();
+  await seedChapters();
+  await seedQuizzes();
+  await seedQuizQuestions();
+  await seedLessons();
+
   await seedSubscriptions();
 
   process.exit(0);
